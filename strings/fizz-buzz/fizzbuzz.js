d368b31deeba567b1af94c5b8a6a922d98f0c98f
pgarbrecht <philgarbrecht@gmail.com>
2022-09-13 07:40:00 -0500

    updated folder structure and notes for fizzbuzz

--- strings/fizz-buzz/fizzbuzz.js
+++ strings/fizz-buzz/fizzbuzz.js
@@ -11,22 +11,25 @@ Output: ["1","2","Fizz","4","Buzz","Fizz","7","8","Fizz","Buzz","11","Fizz","13"
 */
 
 var fizzBuzz = function(n) {
-    let answer = [];
+    let answer = []; //create an answer array
     //iterate from 1 to the number that is passed in
     for (let i = 1; i <= n; i++) {
-        //if i is divisble by 3 but not 5
+        //if i is divisble by 3 but not 5, push Fizz
         if(i%3 == 0 && i%5 !== 0) {
             answer.push("Fizz");
         }
+        //else if i is divisble by 5 and not 3, push Buzz
         else if(i%5 == 0 && i%3 !==0) {
             answer.push("Buzz");
-        }
+        } 
+        //else if i is divisible by 3 and 5, push FizzBuzz
         else if(i%3 == 0 && i%5 == 0) {
             answer.push("FizzBuzz");
         }
+        //if i isn't divisible by 3 or 5, push the number
         else { 
             answer.push(i.toString());
         }
     }
-return answer;
+return answer; //return the answer array
 };
