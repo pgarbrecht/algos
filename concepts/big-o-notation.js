0cc1addee6047eb5db11351513e16a5a13e88aab
pgarbrecht <philgarbrecht@gmail.com>
2022-09-14 17:55:56 -0500

    added more big o explanation

---
+++ concepts/big-o-notation.js
@@ -0,0 +1,8 @@
+/* There are often many ways to solve a problem. Big O is a system to measure efficiency of a code solution. More efficient code is usually better. But "better" can depend on the use case, and Big O can also help you measure trade-offs between different solutions (ie time vs space).
+
+Big O uses numeric representations of how efficient the code is. Using n as the number of simple operations the computer has to perform in a solution, there are a few categories of solutions. They are:
+-
+-
+-
+
+*/
