2ef284e17c8e57424b1cb0b62fb345101605eb2b
pgarbrecht <philgarbrecht@gmail.com>
2022-09-15 17:18:30 -0500

    updated fizzbuzz notes

--- problems/strings/fizz-buzz/fizzbuzz.js
+++ problems/strings/fizz-buzz/fizzbuzz.js
@@ -10,7 +10,7 @@ Input: n = 15
 Output: ["1","2","Fizz","4","Buzz","Fizz","7","8","Fizz","Buzz","11","Fizz","13","14","FizzBuzz"]
 */
 
-var fizzBuzz = function(n) {
+var fizzBuzz = function(n) { //a number is passed in
     let answer = []; //create an answer array
     //iterate from 1 to the number that is passed in
     for (let i = 1; i <= n; i++) {
