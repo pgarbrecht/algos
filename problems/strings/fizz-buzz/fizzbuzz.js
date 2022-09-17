2c99b4263a7ac8432897c390155111c56d34e9a2
pgarbrecht <philgarbrecht@gmail.com>
2022-09-17 17:08:11 -0500

    updated fizzbuzz notes

--- problems/strings/fizz-buzz/fizzbuzz.js
+++ problems/strings/fizz-buzz/fizzbuzz.js
@@ -1,8 +1,8 @@
 /*
 Given an integer n, return a string array answer (1-indexed) where:
-answer[i] == "FizzBuzz" if i is divisible by 3 and 5.
-answer[i] == "Fizz" if i is divisible by 3.
-answer[i] == "Buzz" if i is divisible by 5.
+answer[i] == "FizzBuzz" if i is divisible by 3 and 5 with no remainder.
+answer[i] == "Fizz" if i is divisible by 3 with no remainder.
+answer[i] == "Buzz" if i is divisible by 5 with no remainder.
 answer[i] == i (as a string) if none of the above conditions are true.
 
 Example:
