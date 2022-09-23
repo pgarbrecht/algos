5b11afe3f264c4c04707d6073a9e499608e8f3a1
pgarbrecht <philgarbrecht@gmail.com>
2022-09-22 19:45:31 -0500

    updated valid parentheses

--- problems/strings/valid-parentheses/valid-parentheses.js
+++ problems/strings/valid-parentheses/valid-parentheses.js
@@ -20,22 +20,22 @@ Output: false
 */
 
 var isValid = function(s) {
-    const stack = []; //create empty stack variable
+    const stack = []; //create empty stack array
       
       for (let i = 0; i < s.length; i += 1) { //loop over length of input string
-        const top = stack[stack.length - 1];
-        if (s[i] === '(' || s[i] === '{' || s[i] === '[') {
-          stack.push(s[i]);
-        } else if (s[i] === ')' && top === '(' && stack.length !== 0) {
-          stack.pop();
+        const top = stack[stack.length - 1]; //create variable for top of stack
+        if (s[i] === '(' || s[i] === '{' || s[i] === '[') { //if there is a valid open parenthesis symbol
+          stack.push(s[i]); //then add symbol to stack
+        } else if (s[i] === ')' && top === '(' && stack.length !== 0) { //
+          stack.pop(); //if a ) close tag matching ( open tag is found, empty stack
         } else if (s[i] === ']' && top === '[' && stack.length !== 0) {
-          stack.pop();
+          stack.pop(); //if a ] close tag matching [] open tag is found, empty stack
         } else if (s[i] === '}' && top === '{' && stack.length !== 0) {
-          stack.pop();
+          stack.pop(); //if a } close tag matching { open tag is found, empty stack
         } else {
-          return false;
+          return false; //if none of the above true, return false
         }
       }
       
-      return stack.length === 0;
+      return stack.length === 0; //if input met requirement, stack is no empty and will return "true" as answer, if stack not empty will return false
     };
