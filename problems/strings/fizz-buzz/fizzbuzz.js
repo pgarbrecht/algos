e01def86f479e2cdca039f95ada724d1b0d23e9b
pgarbrecht <philgarbrecht@gmail.com>
2022-12-11 16:38:59 -0600

    refactored fizzbuzz

--- problems/strings/fizz-buzz/fizzbuzz.js
+++ problems/strings/fizz-buzz/fizzbuzz.js
@@ -14,18 +14,19 @@ var fizzBuzz = function(n) { //a number is passed in
     let answer = []; //create an answer array
     //iterate from 1 to the number that is passed in
     for (let i = 1; i <= n; i++) {
+        //if i can be divided by either 3 or 5 with no remainder, push FizzBuzz
+        if(i%3 == 0 && i%5 == 0) {
+            answer.push("FizzBuzz");
+        }
         //if i can be divided by 3 with no remainder but 5 cannot, push Fizz
-        if(i%3 == 0 && i%5 !== 0) {
+        else if(i%3 == 0) {
             answer.push("Fizz");
         }
         //else if i can be divided by 5 with no remainder but 3 cannot, push Buzz
-        else if(i%5 == 0 && i%3 !==0) {
+        else if(i%5 == 0) {
             answer.push("Buzz");
         } 
-        //else if i can be divided by either 3 or 5 with no remainder, push FizzBuzz
-        else if(i%3 == 0 && i%5 == 0) {
-            answer.push("FizzBuzz");
-        }
+        
         //if i can't be divided by 3 or 5 with no remainder, push the number
         else { 
             answer.push(i.toString());
@@ -33,3 +34,6 @@ var fizzBuzz = function(n) { //a number is passed in
     }
 return answer; //return the answer array
 };
+
+//test case
+console.log(fizzBuzz(30))
