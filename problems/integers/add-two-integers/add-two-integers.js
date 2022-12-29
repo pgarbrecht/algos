6616a91be13dea764fd79e01151b23da19da6f2d
pgarbrecht <philgarbrecht@gmail.com>
2022-12-29 10:41:14 -0600

    added add two integers

---
+++ problems/integers/add-two-integers/add-two-integers.js
@@ -0,0 +1,20 @@
+/*
+Given two integers num1 and num2, return the sum of the two integers.
+ 
+Example 1:
+Input: num1 = 12, num2 = 5
+Output: 17
+Explanation: num1 is 12, num2 is 5, and their sum is 12 + 5 = 17, so 17 is returned.
+
+Constraints:
+-100 <= num1, num2 <= 100
+*/
+
+var sum = function(num1, num2) {
+    //create answer variable
+    let answer;
+    //set answer variable equal to sum of both integers
+    answer = num1 + num2;
+    //return answer variable
+    return answer;
+};
