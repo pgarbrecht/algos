ad21b797c87df5ec9e596fd2e92554557a26cfa1
pgarbrecht <philgarbrecht@gmail.com>
2023-01-09 21:43:52 -0600

    added check if two string arrays are equivalent

---
+++ problems/arrays/check-two-string-arrays-equivalent/check-two-string-arrays-equivalent.js
@@ -0,0 +1,35 @@
+/*
+Check If Two String Arrays are Equivalent
+
+Given two string arrays word1 and word2, return true if the two arrays represent the same string, and false otherwise.
+A string is represented by an array if the array elements concatenated in order forms the string.
+
+Example 1:
+Input: word1 = ["ab", "c"], word2 = ["a", "bc"]
+Output: true
+Explanation:
+word1 represents string "ab" + "c" -> "abc"
+word2 represents string "a" + "bc" -> "abc"
+The strings are the same, so return true.
+*/
+
+var arrayStringsAreEqual = function(word1, word2) {
+    //create variables for concatenated words 1 and 2
+    let concatWord1 = '';
+    let concatWord2 = '';
+    //loop through array 1 and concatenate into word 1
+    for(let i = 0; i < word1.length; i++){
+        concatWord1 += word1[i];
+    }
+    //loop through array 2 and concatenate into word 2
+    for(let i = 0; i < word2.length; i++){
+        concatWord2 += word2[i];
+    }
+    //conditional logic
+    //if two words equal, return true
+    if(concatWord1 === concatWord2){
+        return true;
+    } else{ //else, return false
+        return false
+    }
+};
