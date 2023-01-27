6550af6f06734c76141bdafb8d5454a3a757f5b6
pgarbrecht <philgarbrecht@gmail.com>
2023-01-26 20:10:34 -0600

    updated jewels and stones

--- problems/strings/jewels-and-stones/jewels-and-stones.js
+++ problems/strings/jewels-and-stones/jewels-and-stones.js
@@ -1,7 +1,7 @@
 /*
  Jewels and Stones
 
-You're given strings jewels representing the types of stones that are jewels, and stones representing the stones you have. Each character in stones is a type of stone you have. You want to know how many of the stones you have are also jewels.
+You're given a string variable called jewels representing the types of stones that are jewels, and a stones string variable representing the stones you have. Each character in stones is a type of stone you have. You want to know how many of the stones you have are also jewels.
 Letters are case sensitive, so "a" is considered a different type of stone from "A".
  
 Example 1:
@@ -14,8 +14,9 @@ var numJewelsInStones = function(jewels, stones) {
     let answer = 0;
     // loop through stones
     for(let i = 0; i < stones.length; i++){
-        //on each iteration, check if jewels contains the current character in stones, if so increment answer
+        //on each iteration, check if jewels contains the current character in stones,
         if(jewels.includes(stones[i])){
+            // if so increment answer
             answer += 1;
         }
     }
